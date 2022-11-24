b8158ed9355d717175a7b9a72aed1b9ede57461d
Senay Gebrat <senaygebrat@gmail.com>
2022-11-23 16:27:00 -0800

    created User model

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const User = model('user', UserSchema)
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  })
+})
