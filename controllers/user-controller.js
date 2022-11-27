4044d88092476598ebed7db9b371d451201f164c
Senay Gebrat <senaygebrat@gmail.com>
2022-11-26 20:52:10 -0800

    updated put route

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,16 +1,16 @@
 const { User, Thought } = require("../models");
 
 module.exports = {
-  //get all users
+  //get all users, /api/users
   getUsers(req, res) {
     User.find()
-      // .select('-__v')
+      .select('-__v')
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
 
 
-// get a single user
+// get a single user, /api/user/:userid
 getSingleUser(req, res) {
   User.findOne({ _id: req.params.userId })
   .select('-__v')
@@ -27,8 +27,34 @@ createUser(req, res) {
   User.create(req.body)
   .then((user) => res.json(user))
   .catch((err) => res.status(500).json(err));
+},
+
+updateUser(req, res) {
+  User.findOneAndUpdate(
+    { _id: req.params.userId },
+    { $set: req.body },
+    { runValidators: true, new: true }
+  )
+    .then((user) => {
+          if(!user){        
+            res.status(404).json({ message: 'No user with this id!' })
+          }
+          else{res.json(user)
+          }
+})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
+
+
 }
+
+
 };
+
+
 
 // delete a user
 // deleteUser(req, res) {
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const {
   getUsers,
-  createUser
+  createUser,
+  getSingleUser,
+  updateUser
 } = require("../../controllers/user-controller");
 
 
 // /api/users
 router.route("/").get(getUsers).post(createUser);
 // router.route("/").post(createUser);
-
+router.route('/:userId').get(getSingleUser).put(updateUser);
 // /api/user/userId/friends/:friendId
 
 module.exports = router;
